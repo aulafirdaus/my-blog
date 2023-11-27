8ddc031fbf4af2da15412c6dfb30cb4f2f3ca0c9
aulafirdaus <aulafirdaus2945@gmail.com>
2023-11-27 14:13:23 +0700

    pengelompokkan route artikel

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,15 @@ Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/gallery', [PageController::class, 'gallery'])->name('gallery');
 
 # Articles
-Route::get('articles', [ArticleController::class, 'index'])->name('articles');
-Route::get('articles/create', [ArticleController::class, 'create'])->name('articles.create');
-Route::post('articles', [ArticleController::class, 'store'])->name('articles.store');
-Route::get('articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
-Route::put('articles/{article}', [ArticleController::class, 'update'])->name('articles.update');
-Route::get('articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
-Route::delete('articles/{article}', [ArticleController::class, 'destroy'])->name('articles.delete');
+Route::controller(ArticleController::class)->group(function () {
+    Route::get('articles', 'index')->name('articles');
+    Route::get('articles/create', 'create')->name('articles.create');
+    Route::post('articles', 'store')->name('articles.store');
+    Route::get('articles/{article}/edit', 'edit')->name('articles.edit');
+    Route::put('articles/{article}', 'update')->name('articles.update');
+    Route::get('articles/{article}', 'show')->name('articles.show');
+    Route::delete('articles/{article}', 'destroy')->name('articles.delete');
+});
 
 # Users
 Route::get('users', [UserController::class, 'index'])->name('users');
